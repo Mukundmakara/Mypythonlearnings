fb72de6d1fb62ec51bfdcdd6129b573a3c3ca3e4
Mukundmakara <khadelwal8815@gmail.com>
2025-05-07 17:15:11 +0530

    Add files via upload

---
+++ Greeting.py
@@ -0,0 +1,5 @@
+#Personalized Greeting
+first=input("Enter the firstname =")
+last=input("Enter lastname =  ")
+fullname=first + last
+print("Hello,"+fullname +"!Welcome to the Python program")
---
+++ MathsOperations.py
@@ -0,0 +1,12 @@
+#Perform Basic Mathematical Operations
+a=int(input('Enter Number1 :'))
+b=int(input('Enter Number2 :'))
+
+Add = a+b
+Sub = a-b
+Multi = a*b
+divide = a/b
+print("Addition =",Add)
+print("Substraction =",Sub)
+print("Multiplacation =",Multi)
+print("Substraction =",divide)
