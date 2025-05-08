8f2f0e087c56ed99999e2c65a27fd46e7d50c986
Mukundmakara <khadelwal8815@gmail.com>
2025-05-08 14:24:52 +0530

    Add files via upload

---
+++ Even_Odd.py
@@ -0,0 +1,6 @@
+#Check if a Number is Even or Odd
+num = int(input("Enter a number:"))
+if num % 2 == 0:
+    print("This Number is Even")
+else:
+    print("This Number is Odd")
---
+++ Pythonforloop.py
@@ -0,0 +1,11 @@
+#Sum of Integers from 1 to 50 Using a Loop.
+total = 0
+for x in range(1, 51):
+    total  = total + x
+    print("Sum of integer number is :",total)
+
+
+
+
+
+
